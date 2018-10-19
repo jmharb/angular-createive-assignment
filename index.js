2002d7c9622953357dab9fa5d6318bf796ac5b8c
Mat Harb <mharb@ancestry.com>
2018-10-19 11:03:38 -0600

    create custom directive

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const createiveAssignemnt = angular.module('creative-assignment', []);
 createiveAssignemnt.controller('MainController', function($scope) {
-    $scope.test = 'Testing scope';
-    $scope.score = {
-        user: 10,
-        computer: 15
-    }
-});
+        $scope.test = 'Testing scope';
+        $scope.rock = 'ROCK';
+        $scope.paper = 'PAPER';
+        $scope.scissors = 'SCISSORS';
+        $scope.score = {
+            user: 10,
+            computer: 15
+        }
+    })
+    .directive('rpsoption', function() {
+        return {
+            restrict: 'E',
+            replace: 'true',
+            scope: {
+                type: '='
+            },
+            templateUrl: 'rps-option.html'
+        }
+    })
