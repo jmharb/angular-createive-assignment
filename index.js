a5a11bf80dc3e3ce4ac7eb4f1a27467635cb7cdd
Colton <coltongoates@gmail.com>
2018-10-19 18:08:35 +0000

    added functionality for generating random guess, determining winner and updating score.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const createiveAssignemnt = angular.module('creative-assignment', []);
-createiveAssignemnt.controller('MainController', function($scope) {
+const creativeAssignment = angular.module('creative-assignment', []);
+creativeAssignment.controller('MainController', function($scope) {
         $scope.test = 'Testing scope';
         $scope.rock = 'ROCK';
         $scope.paper = 'PAPER';
@@ -7,6 +7,30 @@ createiveAssignemnt.controller('MainController', function($scope) {
         $scope.score = {
             user: 10,
             computer: 15
+        }
+
+        function play(userGuess, computerGuess) {
+            const USER_WINS = 0;
+            const COMP_WINS = 1;
+            const TIE = 2;
+
+            if (userGuess == $scope.rock && computerGuess == $scope.scissors ||
+                userGuess == $scope.scissors && computerGuess == $scope.paper ||
+                userGuess == $scope.paper && computerGuess == $scope.rock) {
+                $scope.score.user++;
+                return USER_WINS;
+            }
+            else if (userGuess == computerGuess) {
+                return TIE;
+            }
+            else {
+                $scope.score.computer++;
+                return COMP_WINS;
+            }
+        }
+
+        function getCompGuess() {
+            return Math.floor(Math.random() * 3);
         }
     })
     .directive('rpsoption', function() {
