68d1731f235464da58135885f667a6d96eddff07
Colton <coltongoates@gmail.com>
2018-10-19 18:27:19 +0000

    updated play function

--- index.js
+++ index.js
@@ -9,28 +9,34 @@ creativeAssignment.controller('MainController', function($scope) {
             computer: 15
         }
 
-        function play(userGuess, computerGuess) {
+        function play() { // FIXME: wire to the "go" button
             const USER_WINS = 0;
             const COMP_WINS = 1;
             const TIE = 2;
-
+            
+            var userGuess = 0; // FIXME: tie user guess to UI event
+            var computerGuess = Math.floor(Math.random() * 3); 
+            var result;
+            
             if (userGuess == $scope.rock && computerGuess == $scope.scissors ||
                 userGuess == $scope.scissors && computerGuess == $scope.paper ||
                 userGuess == $scope.paper && computerGuess == $scope.rock) {
                 $scope.score.user++;
-                return USER_WINS;
+                result = USER_WINS;
             }
             else if (userGuess == computerGuess) {
-                return TIE;
+                result = TIE;
             }
             else {
                 $scope.score.computer++;
-                return COMP_WINS;
+                result = COMP_WINS;
             }
+            
+            displayResult(result);
         }
-
-        function getCompGuess() {
-            return Math.floor(Math.random() * 3);
+        
+        function displayResult(result) {
+            // FIXME: needs implementation
         }
     })
     .directive('rpsoption', function() {
